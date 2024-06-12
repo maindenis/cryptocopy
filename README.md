7a5aa7b6813a5509a02a4a7df374883bd6de59e0
maindenis <165322876+maindenis@users.noreply.github.com>
2024-06-12 20:41:58 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# https://maindenis.github.io/cryptocopy/index.html
+# https://maindenis.github.io/cryptocopy/30.html
+# https://maindenis.github.io/cryptocopy/32.html
+# https://maindenis.github.io/cryptocopy/35.html
---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+$(window).resize(function() {
+
+});
+
+$(document).scroll(function() {
+
+});
+
+$(document).ready(function() {
+
+    // --------------
+
+    $(".respmenubtn").click(function(e) {
+      e.preventDefault();
+      if( $("#resp_nav").is(":hidden") ) {
+          $("#resp_nav").fadeIn(300);
+          $(this).addClass("active");
+      } else {
+          $("#resp_nav").fadeOut(300);
+          $(this).removeClass("active");
+      }
+    });
+    
+    $(this).keydown(function(eventObject){
+        if (eventObject.which == 27 &&
+            $("#resp_nav").is(":visible") &&
+            bodyWidth <= 767) {
+                $("#resp_nav").fadeOut(300);
+                $(".respmenubtn").removeClass("active");
+        }
+    });
+
+});
