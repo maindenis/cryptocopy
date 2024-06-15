2d2c12bed1df8955258996ef676f96ffb89bff8c
maindenis <165322876+maindenis@users.noreply.github.com>
2024-06-15 05:29:49 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,4 @@
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-$(window).resize(function() {
-
-});
-
-$(document).scroll(function() {
-
-});
-
 $(document).ready(function() {
-
-    // --------------
 
     $(".respbtn").click(function(e) {
       e.preventDefault();
